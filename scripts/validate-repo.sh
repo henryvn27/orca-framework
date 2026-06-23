a4924e6ab23e86ecdc1565da36c282c0a729c983
henryvn27 <henryvanness3@gmail.com>
2026-06-23 11:39:48 -0400

    Document outbox JSON ok field

--- scripts/validate-repo.sh
+++ scripts/validate-repo.sh
@@ -90,6 +90,7 @@ need_grep_file "orca goal --packs" README.md
 need_grep_file "orca goal --packs --verbose" README.md
 need_grep_file "orca notion handoff --issue" README.md
 need_grep_file "orca notion handoff --json" README.md
+need_grep_file "orca notion outbox --json.*ok:true" README.md
 
 [ -x bin/orca ] || fail "bin/orca is not executable"
 [ -x install/install.sh ] || fail "install/install.sh is not executable"
